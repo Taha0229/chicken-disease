8a2f9446ba7a9dc501f4bc54d1d49ab87e96b5a8
Taha0229 <sheikhtahamaroof@gmail.com>
2024-01-31 16:09:09 +0530

    Feat-prepare base model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chickenDiseaseClassifier import logger
 from chickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chickenDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -8,6 +9,19 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n x===============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f"*********************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n x===============x")
 except Exception as e:
     logger.exception(e)
